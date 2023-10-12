95a70d4595a035b9a41a41e001749b9f7a3fa8a0
Clement Monier <clement.monier@bodet-timesport.com>
2023-10-12 13:58:37 +0200

    Ajout configuration du build en mode offline

--- jenkins-yocto-build.sh
+++ jenkins-yocto-build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 EULA=1 MACHINE=$1 DISTRO=$2 source setup-environment build
+echo "DL_DIR = \"/opt/yocto-downloads/\"" >> conf/local.conf
+echo "SSTATE_DIR = \"/opt/$1-$2-$3-sstate/\"" >> conf/local.conf
+echo "BB_NO_NETWORK = \"1\"" >> conf/local.conf
 bitbake $3
