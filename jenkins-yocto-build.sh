ae13a6b5c37833e5ba248aab130e4435a5a74418
Clement Monier <clement.monier@bodet-timesport.com>
2023-10-18 16:57:51 +0200

    update pour jenkins

--- jenkins-yocto-build.sh
+++ jenkins-yocto-build.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+find_and_replace_or_create () {
+    correct_string=$(echo ${2} | sed 's/\//\\\//g')
+    case `grep '^'${1}'' conf/local.conf >/dev/null; echo $?` in
+    0)
+        # code if found : replace in file
+        sed -i -e 's/^'${1}'.*/'${1}' = \"'${correct_string}'\"/' conf/local.conf
+        ;;
+    1)
+        # code if not found : append to end of file
+        echo "${1} = \"${2}\"" >> conf/local.conf
+        ;;
+    *)
+        # code if an error occurred
+        echo "ERROR: grep"
+        exit 1
+        ;;
+    esac   
+}
+
 EULA=1 MACHINE=$1 DISTRO=$2 source setup-environment build
+find_and_replace_or_create DL_DIR "/opt/yocto-downloads"
+find_and_replace_or_create SSTATE_DIR "/opt/yocto-downloads"
+find_and_replace_or_create UNINATIVE_URL "http://tremosiris.trem.bod:8081/repository/Scorepad/yocto-haumea/"
+find_and_replace_or_create SOURCE_MIRROR_URL "http://tremosiris.trem.bod:8081/repository/Scorepad/yocto-haumea/"
+find_and_replace_or_create BB_FETCH_PREMIRRORONLY "1"
+find_and_replace_or_create BB_NO_NETWORK "1"
 echo "INHERIT += \"own-mirrors\"" >> conf/local.conf
-echo "SOURCE_MIRROR_URL = \"http://tremosiris.trem.bod:8081/repository/Scorepad/yocto-haumea/"" >> conf/local.conf
-echo "DL_DIR = \"/opt/yocto-downloads/\"" >> conf/local.conf
-echo "BB_NO_NETWORK = \"1\"" >> conf/local.conf
-echo "SSTATE_DIR = \"/opt/yocto-$1-$2-$3-sstate/\"" >> conf/local.conf
-echo "BB_GENERATE_MIRROR_TARBALLS = \"1\"" >> conf/local.conf
 bitbake $3
