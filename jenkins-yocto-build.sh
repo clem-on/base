2be75e83f918def88bb44ae1f548438f5d4fdc80
Clement Monier <clement.monier@bodet-timesport.com>
2023-10-18 13:37:00 +0200

    update pour jenkins

--- jenkins-yocto-build.sh
+++ jenkins-yocto-build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 EULA=1 MACHINE=$1 DISTRO=$2 source setup-environment build
+echo "INHERIT += \"own-mirrors\"" >> conf/local.conf
+echo "SOURCE_MIRROR_URL = \"http://tremosiris.trem.bod:8081/repository/Scorepad/yocto-haumea/"" >> conf/local.conf
 echo "DL_DIR = \"/opt/yocto-downloads/\"" >> conf/local.conf
-echo "SSTATE_DIR = \"/opt/yocto-sstate/\"" >> conf/local.conf
 echo "BB_NO_NETWORK = \"1\"" >> conf/local.conf
+echo "SSTATE_DIR = \"/opt/yocto-$1-$2-$3-sstate/\"" >> conf/local.conf
+echo "BB_GENERATE_MIRROR_TARBALLS = \"1\"" >> conf/local.conf
 bitbake $3
